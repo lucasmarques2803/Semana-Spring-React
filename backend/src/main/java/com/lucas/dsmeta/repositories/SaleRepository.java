7e278d106434e875eaa34468834d3b8d442421ff
Lucas Marques <lucas-marques2003@hotmail.com>
2022-09-11 18:05:08 -0300

    Teste da API

---
+++ backend/src/main/java/com/lucas/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.lucas.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lucas.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
