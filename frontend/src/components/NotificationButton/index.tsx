f88206410beaacc8be6e1e172e53dc77a5af3926
Lucas Marques <lucas-marques2003@hotmail.com>
2022-09-11 23:49:15 -0300

    Mandar notificação

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,11 +1,25 @@
 import icon from "../../assets/img/notification-icon.svg"
+import axios from 'axios'
 
 import "./styles.css"
+import { BASE_URL } from "../../utils/request"
 
-function NotificationButton() {
+type Props = {
+    saleId: number
+
+}
+
+function handleClick(saleId: number) {
+    axios(`${BASE_URL}/sales/${saleId}/notification`)
+        .then(response => {
+            console.log("SUCESSO")
+        })
+}
+
+function NotificationButton({ saleId }: Props) {
     return (
-        <div className="dsmeta-red-btn">
-            <img src={icon} alt="Notificar"/>
+        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
+            <img src={icon} alt="Notificar" />
         </div>
     )
 }
